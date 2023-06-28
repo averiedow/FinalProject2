aebca043c4b2e3461fd87ca664b7a9532eadcef3
Averie Dow <averie.addington@gmail.com>
2023-06-28 14:25:25 -0600

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Averie Dow on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
