51e296af8afbe0d631256e372463b3a2ab8ebe91
Ella <ellaclaire2@icloud.com>
2023-06-28 15:44:01 -0500

    cheshire

--- FinalProject/ContentView.swift
+++ FinalProject/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("cheshire")
             Text("making changes")
             Text("making another change")
         }
