1df7902e63bb297b5b2976d12a58cbe3c4673536
Averie Dow <averie.addington@gmail.com>
2023-06-30 14:21:40 -0600

    ADD TO LIBRARY BUTTONS

--- FinalProject/AddToLibrary.swift
+++ FinalProject/AddToLibrary.swift
@@ -7,23 +7,57 @@
 
 import SwiftUI
 
-struct AddToLibrary: View {
-    struct Books: Identifiable, Hashable {
-        
-        var name: String
-        var author: String
-        var image: String
-        var description: String
-        let id = UUID()
-    }
 
+
+struct AddToLibrary: View {
+    @State var textFieldText: String = ""
+    @State var bookAuthorText : String = ""
+    @State var opinionsText : String = ""
     var body: some View {
-        LazyHStack{
-            Text("Book Title")
-            TextField(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/, text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack{
+                HStack{
+                    Text("Book Title")
+                        .font(.headline)
+                    TextField("Type Here", text: $textFieldText)
+                        .background(Color.orange.opacity(0.3).cornerRadius(8))
+                        .foregroundColor(.black)
+                        .font(.headline)
+                }.padding()
+                HStack{
+                    Text("Book Author")
+                        .font(.headline)
+                    TextField("Type Here", text: $bookAuthorText)
+                        .background(Color.orange.opacity(0.3).cornerRadius(8))
+                        .foregroundColor(.black)
+                        .font(.headline)
+                }.padding()
+                HStack{
+                    Text("How do you like it?")
+                        .font(.headline)
+                    TextField("Type your thoughts!", text: $opinionsText)
+                        .background(Color.orange.opacity(0.3).cornerRadius(8))
+                        .foregroundColor(.black)
+                        .font(.headline)
+                }.padding()
+                    
+                                    Button(action: {
+                
+                                    }, label:{
+                                        Text("Save Book".uppercased())
+                                            .padding()
+                                            .background(Color.orange.opacity(0.3).cornerRadius(8))
+                                            .foregroundColor(.black)
+                                            .font(.headline)
+                
+                                    })
+                }
+                
+            }
+        
+        
         }
     }
-}
 
 struct AddToLibrary_Previews: PreviewProvider {
     static var previews: some View {
